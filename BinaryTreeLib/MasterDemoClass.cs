82f6e2fc5434df52fb1ad207a308ac9568c01cac
Dawid Biel <dawidbiell@gmail.com>
2022-01-24 23:18:10 +0100

    added clas to master branch

---
+++ BinaryTreeLib/MasterDemoClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeLib
+{
+    internal class MasterDemoClass
+    {
+        public int MyNumber { get; set; }
+
+        public void myMethod()
+        {
+            MyNumber++;
+        }
+    }
+}
